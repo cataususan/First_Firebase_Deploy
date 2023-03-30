dab7fbcf6cc5c7266b3e93cb47f44f315dc21a59
cataususan <cataususan@gmail.com>
2023-03-30 17:21:55 +0300

    da

---
+++ angular-firebase-hosting/src/app/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAgOEpfOSgStny3VNx3sbtQVG01YQWYBw4',
+    authDomain: 'fir-test-application-785f4.firebaseapp.com',
+    projectId: 'fir-test-application-785f4',
+    storageBucket: 'fir-test-application-785f4.appspot.com',
+    messagingSenderId: '485560230087',
+    appId: '1:485560230087:web:d2a11068206ea0c8739ee2',
+    measurementId: 'G-QYSF4R1C2E',
+  },
+};
